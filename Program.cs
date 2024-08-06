8828061b55c71d445be7e6cd48a4f9cf973bb936
Natasha Buckham <natashaisobel@gmail.com>
2024-08-06 15:39:23 +0100

    Implements CalculateAmountOwed and ListAllTransactions methods. Co-authored-by: Dorothy Wong <dorothyynwong@users.noreply.github.com>

--- Program.cs
+++ Program.cs
@@ -7,11 +7,11 @@ public class Program
         string fileName = "./Files/Transactions2014.csv";
         Extractor extractor = new Extractor { FileName = fileName };
         (List<Person>, List<Transaction>) data = extractor.ExtractData();
-        foreach(var person in data.Item1) Console.WriteLine(person.Name);
-        foreach(var transaction in data.Item2) Console.WriteLine(transaction.Amount);
+        // foreach(var person in data.Item1) Console.WriteLine(person.Name);
+        // foreach(var transaction in data.Item2) Console.WriteLine(transaction.Amount);
         Report report = new Report(data.Item1, data.Item2);
-        Person testPerson = new Person {Id = 1, Name = "Jon A"};
-        Console.WriteLine(report.CalculateAmountLent(testPerson));
-        
+        // Person testPerson = new Person {Id = 1, Name = "Jon A"};
+        // Console.WriteLine(report.CalculateAmountLent(testPerson));
+        report.ListAllTransactions();
     }
 }
--- Report.cs
+++ Report.cs
@@ -12,7 +12,17 @@ public class Report
 
     public void ListAllTransactions ()
     {
-        Console.WriteLine("list of transactions");
+        foreach(Person person in People)
+        {
+            Console.WriteLine($"Name: {person.Name}");
+            double lentValue = CalculateAmountLent(person);
+            string amountLent = String.Format("{0:0.00}", Math.Round(lentValue, 2));
+            Console.WriteLine($"Total amount lent: {amountLent}");
+            double owedValue = CalculateAmountOwed(person);
+            string amountOwed = String.Format("{0:0.00}", Math.Round(owedValue, 2));
+            Console.WriteLine($"Total amount lent: {amountOwed}");
+        }
+
     }
 
     public void ListTransactionsByPerson(Person person)
@@ -24,12 +34,13 @@ public class Report
         double total = 0;
         List<Transaction> fromTransactions = Transactions.FindAll(transaction => transaction.From.Name == person.Name);
         foreach(Transaction fromTransaction in fromTransactions) total += fromTransaction.Amount;
-        total = Math.Round(total, 2);
         return total;
     }
 
     public double CalculateAmountOwed(Person person) {
         double total = 0;
+        List<Transaction> toTransactions = Transactions.FindAll(transaction => transaction.To.Name == person.Name);
+        foreach(Transaction toTransaction in toTransactions) total += toTransaction.Amount;
         return total;
     }
 
